97aa8683d90cf8f8cb7f69ef2cf9ed5d465b9c68
joji <jojijoseph@iisc.ac.in>
2021-06-19 02:08:05 +0530

    SAC: Refactored play.py

--- SAC/play.py
+++ SAC/play.py
@@ -44,14 +44,15 @@ try:
         eval_env.render()
 except:
     pass
-n_episodes = 1
-if eval: n_episodes = 100
-done = False
-total = 0
+
+n_episodes = 100 if eval else 1
+
+
 returns = []
 for episode in range(n_episodes):
     state = eval_env.reset()
     done = False
+    total_return = 0
     while not done:
         with torch.no_grad():
             state = state[None,:]
@@ -62,10 +63,10 @@ for episode in range(n_episodes):
                 eval_env.render()
                 time.sleep(1/fps)
             state, reward, done,_ = eval_env.step(action*ACTION_SCALE)
-            total += reward
-    returns.append(total)
-    print(episode+1, total)
-    total = 0
+            total_return += reward
+    returns.append(total_return)
+    print(f"Episode: {episode+1}, Return: {total_return}")
+    total_return = 0
 
 print("best", np.max(returns))
 print("mean:", np.mean(returns))
