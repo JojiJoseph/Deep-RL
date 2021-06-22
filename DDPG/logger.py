b1c9c092d99d4922c00166cb7c914ae94a006ce0
joji <jojijoseph@iisc.ac.in>
2021-06-22 23:44:04 +0530

    DDPG: Improved Logger

--- DDPG/logger.py
+++ DDPG/logger.py
@@ -1,6 +1,6 @@
 class Logger:
     @staticmethod
     def print(key, value):
-        print("| %20s | %20s |"%(key,value))
+        print("| %20s | %20.20s |" % (key,value))
     def print_boundary():
         print("-"*47)
