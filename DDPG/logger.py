1556135abd4e3edf6f7eb79763becdeacc270c0e
joji <jojijoseph@iisc.ac.in>
2021-06-23 18:01:01 +0530

    DDPG: Improved DDPG Logger. TODO: change boundary() commands in logger.py to top_rule, mid_rule etc

--- DDPG/logger.py
+++ DDPG/logger.py
@@ -2,5 +2,9 @@ class Logger:
     @staticmethod
     def print(key, value):
         print("| %20s | %20.20s |" % (key,value))
+    def print_title(title):
+        print("|  %-42s |" % title)
     def print_boundary():
         print("-"*47)
+    def print_double_boundary():
+        print("|" + "="*45 + "|")
